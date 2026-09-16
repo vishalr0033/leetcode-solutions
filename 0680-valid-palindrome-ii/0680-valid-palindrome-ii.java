03f0d074ebfa68a0c8b668235454db9cb7f781c4
Vishal R <vishalr0033@gmail.com>
2026-09-16 23:27:08 +0530

    Time: 4 ms (98.36%), Space: 48 MB (13.04%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right)){
+                return ispalin(s,left+1,right) || ispalin(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    static boolean ispalin(String a,int left,int right){
+        while(left<right){
+            if(a.charAt(left)!=a.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
