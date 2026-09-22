b8fb4a2786bda4f970de52f0ff4fd88954f5e210
Vishal R <vishalr0033@gmail.com>
2026-09-22 23:05:57 +0530

    Time: 8 ms (73.59%), Space: 65.3 MB (40.5%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        fun(0,s,new ArrayList<>(),ans);
+        fun(0,new ArrayList<>(),ans,s);
         return ans;
     }
-    static void fun(int index,String a,List<String> list,List<List<String>> ans){
-        if(index == a.length()){
-            ans.add(new ArrayList<>(list));
+    static void fun(int index,List<String> curr,List<List<String>> list,String s){
+        if(index==s.length()){
+            list.add(new ArrayList<>(curr));
             return;
         }
-        for(int i=index;i<a.length();i++){
-            if(ispalin(a,index,i)){
-                list.add(a.substring(index,i+1));
-                fun(i+1,a,list,ans);
-                list.remove(list.size()-1);
+        for(int i=index;i<s.length();i++){
+            if(isPalin(index,i,s)){
+                curr.add(s.substring(index,i+1));
+                fun(i+1,curr,list,s);
+                curr.remove(curr.size()-1);
             }
         }
     }
-    static boolean ispalin(String a , int start, int end){
-        while(start <= end){
-            if(a.charAt(start)!=a.charAt(end)){
+    static boolean isPalin(int i,int j,String s){
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
                 return false;
             }
-            start++;
-            end--;
+            i++;
+            j--;
         }
         return true;
     }
