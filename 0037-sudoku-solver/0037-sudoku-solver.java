d6f8779ef17f4787b4561cedded2ed3716535deb
Vishal R <vishalr0033@gmail.com>
2026-09-22 22:32:21 +0530

    Time: 386 ms (29.9%), Space: 43 MB (25.81%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -3,11 +3,11 @@ class Solution {
         solve(board);
     }
     static boolean solve(char[][] board){
-        for(int i = 0;i < board.length;i++){
-            for(int j = 0;j < board[0].length;j++){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='.'){
-                    for(char k='1';k<='9';k++){
-                        if(isValid(board,i,j,k)){
+                    for(char k = '1';k <= '9';k++){
+                        if(isSafe(board,i,j,k)){
                             board[i][j] = k;
                             if(solve(board)){
                                 return true;
@@ -22,11 +22,11 @@ class Solution {
         }
         return true;
     }
-    static boolean isValid(char[][] b,int row ,int col ,char k){
+    static boolean isSafe(char[][] board,int row,int col,char k){
         for(int i=0;i<9;i++){
-            if(b[i][col]==k) return false;
-            if(b[row][i]==k) return false;
-            if(b[3*(row/3)+i/3][3*(col/3)+i%3]==k) return false;
+            if(board[i][col]==k) return false;
+            if(board[row][i]==k) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==k) return false;
         }
         return true;
     }
