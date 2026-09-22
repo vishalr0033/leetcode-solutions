084844b89461d96a53b37ade90bdbb7d6f424549
Vishal R <vishalr0033@gmail.com>
2026-09-22 10:31:37 +0530

    Time: 1 ms (87.88%), Space: 45.5 MB (46.18%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -5,18 +5,18 @@ class Solution {
         fun(nums,new ArrayList<>(),ans,bol);
         return ans;
     }
-    static void fun(int[] arr,List<Integer> ds,List<List<Integer>> list,boolean[] bol){
-        if(arr.length == ds.size()){
-            list.add(new ArrayList<>(ds));
+    static void fun(int []arr,List<Integer> curr,List<List<Integer>> ans,boolean[] bol){
+        if(arr.length==curr.size()){
+            ans.add(new ArrayList<>(curr));
             return;
         }
         for(int i=0;i<arr.length;i++){
             if(!bol[i]){
                 bol[i] = true;
-                ds.add(arr[i]);
-                fun(arr,ds,list,bol);
-                ds.remove(ds.size()-1);
-                bol[i] = false;
+                curr.add(arr[i]);
+                fun(arr,curr,ans,bol);
+                curr.remove(curr.size()-1);
+                bol[i]=false;
             }
         }
     }
