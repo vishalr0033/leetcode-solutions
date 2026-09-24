846970a994251cf1f46a16eb73c70ebe082fdf34
Vishal R <vishalr0033@gmail.com>
2026-09-24 22:24:17 +0530

    Time: 2 ms (81.92%), Space: 46.7 MB (27.98%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,57 +1,52 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char board[][] = new char[n][n];
+        List<List<String>> list = new ArrayList<>();
+        char[][] board = new char[n][n];
         for(int i=0;i<n;i++){
             Arrays.fill(board[i],'.');
         }
-        fun(0,board,ans,n);
-        return ans;
+        fun(0,board,list,n);
+        return list;
     }
-    static void fun(int col,char[][] board,List<List<String>> list ,int n){
-        if(col==n){
+    static void fun(int col,char[][] board,List<List<String>> list,int n){
+        if(col==board.length){
             List<String> li = new ArrayList<>();
             for(int i=0;i<n;i++){
                 li.add(new String(board[i]));
-            }
+            } 
             list.add(li);
             return;
+            
         }
         for(int row = 0;row < n;row++){
-            if(isValid(row,col,board,n)){
-                board[row][col] = 'Q';
-                fun(col+1,board,list,n);
-                board[row][col]='.';
+                if(isValid(row,col,board,n)){
+                    board[row][col] = 'Q';
+                    fun(col+1,board,list,n);
+                    board[row][col] = '.';
+                }
             }
-        }
     }
-    static boolean isValid(int row , int col , char[][] b,int n){
-        int rd = row;
-        int cd = col;
-        while(row >= 0 && col >= 0){
-            if(b[row][col]=='Q'){
-                return false;
-            }
-            row--;
-            col--;
+    static boolean isValid(int r,int c,char[][] board,int n){
+        int rd = r;
+        int cd = c;
+        while(r >= 0  && c >= 0){
+            if(board[r][c]=='Q') return false;
+            r--;
+            c--;
         }
-        row = rd;
-        col = cd;
-        while(col >= 0){
-            if(b[row][col]=='Q'){
-                return false;
-            }
-            col--;
+        r = rd;
+        c = cd;
+        while(c >= 0){
+            if(board[r][c]=='Q') return false;
+            c--;
         }
-        row = rd;
-        col = cd;
-        while( row < n && col >= 0){
-            if(b[row][col]=='Q'){
-                return false;
-            }
-            row++;
-            col--;
-        }
+        r = rd;
+        c = cd;
+        while(r < n && c >= 0){
+            if(board[r][c]=='Q') return false;
+            r++;
+            c--;
+        } 
         return true;
     }
 }
