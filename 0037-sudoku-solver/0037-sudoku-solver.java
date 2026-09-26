b1c5983a1a7401b05f579ecbff68ba461de79075
Vishal R <vishalr0033@gmail.com>
2026-09-26 21:44:55 +0530

    Time: 390 ms (22.95%), Space: 43 MB (26.28%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -4,10 +4,10 @@ class Solution {
     }
     static boolean solve(char[][] board){
         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
+            for(int j= 0;j<board[0].length;j++){
                 if(board[i][j]=='.'){
-                    for(char k = '1';k <= '9';k++){
-                        if(isSafe(board,i,j,k)){
+                    for(char k='1';k<='9';k++){
+                        if(isValid(board,i,j,k)){
                             board[i][j] = k;
                             if(solve(board)){
                                 return true;
@@ -22,7 +22,7 @@ class Solution {
         }
         return true;
     }
-    static boolean isSafe(char[][] board,int row,int col,char k){
+    static boolean isValid(char[][] board,int row,int col,char k){
         for(int i=0;i<9;i++){
             if(board[i][col]==k) return false;
             if(board[row][i]==k) return false;
